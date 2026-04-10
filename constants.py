3859864c6e8138808c02c581b16ccf22be6a4ae2
annarailton <anna.railton@gmail.com>
2026-04-10 17:06:00 +0100

    Add some constants

---
+++ constants.py
@@ -0,0 +1,9 @@
+"""Shared constants for the Oxford planning scraper."""
+
+BASE_URL = "https://public.oxford.gov.uk"
+WEEKLY_LIST_URL = (
+    "https://public.oxford.gov.uk/online-applications/search.do?action=weeklyList"
+)
+RESULTS_URL = "https://public.oxford.gov.uk/online-applications/weeklyListResults.do?action=firstPage"
+DEFAULT_WARD_NAME = "Hinksey Park"
+DEFAULT_TIMEOUT_SECONDS = 30
